33af08525c85529d5d18c39c71e05f6913ba36ce
dev <dev@mac-509117.local>
2019-09-05 20:36:00 +0200

    Updates

---
+++ precache-manifest.1c1874020f75b0995c753030d3899106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab454b180a660c33c1971b41c8c86da5",
+    "url": "/gvg-search-box/index.html"
+  },
+  {
+    "revision": "c86c7be74d0e6a760ada",
+    "url": "/gvg-search-box/static/css/main.73ef9533.chunk.css"
+  },
+  {
+    "revision": "e5dbbfd663c3769d9e5f",
+    "url": "/gvg-search-box/static/js/2.ef7d0da5.chunk.js"
+  },
+  {
+    "revision": "c86c7be74d0e6a760ada",
+    "url": "/gvg-search-box/static/js/main.a43c390b.chunk.js"
+  },
+  {
+    "revision": "853215e26c029f1ac110",
+    "url": "/gvg-search-box/static/js/runtime~main.e9556a56.js"
+  }
+]);
